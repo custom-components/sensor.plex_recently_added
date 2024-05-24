465b9f4963354f3d26d1a5f213bf0c543c5f500f
Makhuta <matykubla@gmail.com>
2024-05-24 15:20:52 +0200

    Fix
    - hopefully that time it is gonna work

--- custom_components/plex_recently_added/redirect.py
+++ custom_components/plex_recently_added/redirect.py
@@ -1,6 +1,6 @@
 from homeassistant.components.http import HomeAssistantView
 from homeassistant.config_entries import ConfigEntry
-from aiohttp import web
+from aiohttp import web, ClientSession
 import requests
 
 from homeassistant.const import (
@@ -8,6 +8,7 @@ from homeassistant.const import (
     CONF_NAME, 
     CONF_HOST, 
     CONF_PORT, 
+    CONF_SSL
     )
 
 from .const import DOMAIN
@@ -18,12 +19,18 @@ class ImagesRedirect(HomeAssistantView):
     def __init__(self, config_entry: ConfigEntry):
         super().__init__()
         self._token = config_entry.data[CONF_API_KEY]
-        self._base_url = f'https://{config_entry.data[CONF_HOST]}:{config_entry.data[CONF_PORT]}'
+        self._base_url = f'http{'s' if config_entry.data[CONF_SSL] else ''}://{config_entry.data[CONF_HOST]}:{config_entry.data[CONF_PORT]}'
         self.name = f'{self._token}_Plex_Recently_Added'
         self.url = f'/{config_entry.data[CONF_NAME].lower() + "_" if len(config_entry.data[CONF_NAME]) > 0 else ""}plex_recently_added'
 
     async def get(self, request):
         path = request.query.get("path", "")
         url = f'{self._base_url}{path}?X-Plex-Token={self._token}'
-        return web.HTTPFound(url)
 
+        async with ClientSession() as session:
+            async with session.get(url) as res:
+                if res.ok:
+                    content = await res.read()
+                    return web.Response(body=content, content_type=res.content_type)
+
+                return web.HTTPNotFound()
