59ed1a314dced5123be2eac4f84938427543f89d
Makhuta <matykubla@gmail.com>
2024-05-24 10:29:10 +0200

    Fix
    - ssl issue with HomeAssistant redirect (from secured HA to unsecured Plex)

--- custom_components/plex_recently_added/redirect.py
+++ custom_components/plex_recently_added/redirect.py
@@ -8,7 +8,6 @@ from homeassistant.const import (
     CONF_NAME, 
     CONF_HOST, 
     CONF_PORT, 
-    CONF_SSL
     )
 
 from .const import DOMAIN
@@ -19,7 +18,7 @@ class ImagesRedirect(HomeAssistantView):
     def __init__(self, config_entry: ConfigEntry):
         super().__init__()
         self._token = config_entry.data[CONF_API_KEY]
-        self._base_url = f'http{'s' if config_entry.data[CONF_SSL] else ''}://{config_entry.data[CONF_HOST]}:{config_entry.data[CONF_PORT]}'
+        self._base_url = f'https://{config_entry.data[CONF_HOST]}:{config_entry.data[CONF_PORT]}'
         self.name = f'{self._token}_Plex_Recently_Added'
         self.url = f'/{config_entry.data[CONF_NAME].lower() + "_" if len(config_entry.data[CONF_NAME]) > 0 else ""}plex_recently_added'
 
