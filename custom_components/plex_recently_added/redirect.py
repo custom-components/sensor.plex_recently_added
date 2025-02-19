e36b64b578963a0c17973ecb30436a78eae795aa
Makhuta <matykubla@gmail.com>
2025-02-19 21:07:42 +0100

    Fix
    - removed redirection from homeassistant to plex
    - made possible to cache images from plex and then distribute them directly from HA

--- custom_components/plex_recently_added/redirect.py
+++ custom_components/plex_recently_added/redirect.py
@@ -2,6 +2,10 @@ from homeassistant.components.http import HomeAssistantView
 from homeassistant.config_entries import ConfigEntry
 from aiohttp import web, ClientSession
 import requests
+import os
+
+from .parser import CACHE_FOLDER
+os.makedirs(CACHE_FOLDER, exist_ok=True)
 
 from homeassistant.const import (
     CONF_API_KEY, 
@@ -19,20 +23,18 @@ class ImagesRedirect(HomeAssistantView):
     def __init__(self, config_entry: ConfigEntry):
         super().__init__()
         self._token = config_entry.data[CONF_API_KEY]
-        self._base_url = f'http{'s' if config_entry.data[CONF_SSL] else ''}://{config_entry.data[CONF_HOST]}:{config_entry.data[CONF_PORT]}'
         self.name = f'{self._token}_Plex_Recently_Added'
         self.url = f'/{config_entry.data[CONF_NAME].lower() + "_" if len(config_entry.data[CONF_NAME]) > 0 else ""}plex_recently_added'
 
     async def get(self, request):
-        metadataId = int(request.query.get("metadata", 0))
-        thumbId = int(request.query.get("thumb", 0))
-        artId = int(request.query.get("art", 0))
-        url = f'{self._base_url}/library/metadata/{metadataId}/{"thumb" if thumbId != 0 else "art"}/{thumbId if thumbId != 0 else artId if artId != 0 else ""}?X-Plex-Token={self._token}'
+        filename = request.query.get("filename")
+        if not filename:
+            return web.Response(status=404)
+        file_path = os.path.join(CACHE_FOLDER, filename)
 
-        async with ClientSession() as session:
-            async with session.get(url) as res:
-                if res.ok:
-                    content = await res.read()
-                    return web.Response(body=content, content_type=res.content_type)
+        if not os.path.isfile(file_path):
+            return web.Response(status=404)
 
-                return web.HTTPNotFound()
+        return web.FileResponse(file_path)
+
+
